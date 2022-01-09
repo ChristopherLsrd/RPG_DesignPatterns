8201654a5457e7573cd5c4a763acef21d2dc793e
ChristopherLsrd <christopher.lessirard@gmail.com>
2022-01-09 18:18:04 +0100

    WIP main 2

--- src/main/App.java
+++ src/main/App.java
@@ -38,6 +38,66 @@ public class App {
         mainPerso.equipStuff();
         initObserver(mainPerso);
 
+        int i=0;
+        int chooseAction;
+        while(i<ennemies.size() && !mainPerso.getEtat().getClass().getName().equals("EtatMort")){
+            Personnage ennemiEnCours=ennemies.get(i);
+            while (!ennemiEnCours.getEtat().getClass().getName().equals("EtatMort") && !mainPerso.getEtat().getClass().getName().equals("EtatMort")){
+                System.out.println("Nouveau tour :");
+                chooseAction=0;
+                do{
+                    System.out.println("Choisissez votre action: ");
+                    System.out.println("1. Attaquer");
+                    System.out.println("2. Lancer un sort");
+                    System.out.println("3. Régénerer votre mana");
+                    System.out.println("4. Vous soigner");
+                    chooseAction=sc.nextInt();
+                    if(chooseAction!=1 && chooseAction!=2 && chooseAction!=3 && chooseAction!=4){
+                        System.out.println("Ce choix ne correspond à aucune action");
+                    }
+                }while(chooseAction!=1 && chooseAction!=2 && chooseAction!=3 && chooseAction!=4);
+                switch (chooseAction){
+                    case 1:
+                        mainPerso.attack(ennemiEnCours);
+                    case 2:
+                        mainPerso.spell(ennemiEnCours);
+                    case 3:
+                        mainPerso.regenMana();
+                    case 4:
+                        mainPerso.heal();
+                }
+
+                if(!ennemiEnCours.getEtat().getClass().getName().equals("EtatMort")){
+                    ennemiEnCours.attack(mainPerso);
+                }
+
+            }
+            if(ennemiEnCours.getEtat().getClass().getName().equals("EtatMort")){
+                i++;
+                if(i<ennemies.size()){
+                    System.out.println(ennemiEnCours.getClass().getName()+" est mort ! un autre ennemi arrive !");
+                }
+                else{
+                    System.out.println(ennemiEnCours.getClass().getName()+" est mort !");
+                }
+
+
+            }
+            else if(mainPerso.getEtat().getClass().getName().equals("EtatMort")){
+                System.out.println("Votre "+ mainPerso.getClass().getName() +"est mort");
+                System.exit(0);
+            }
+        }
+
+        if(mainPerso.getEtat().getClass().getName().equals("EtatMort")){
+            System.out.println("Votre "+ mainPerso.getClass().getName() +"est mort");
+            System.exit(0);
+        }else{
+            System.out.println("Vous avez gagné !");
+            System.exit(0);
+        }
+
+        System.out.println(mainPerso.toString());
     }
 
     private static ArrayList<Personnage> initEnnemies() {
